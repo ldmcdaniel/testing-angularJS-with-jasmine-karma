37115299a1c768cfff03db2173f9167dc2da93b1
Lucas McDaniel <ldmcdaniel@gmail.com>
2016-12-26 22:29:06 -0600

    Part 1 complete

---
+++ app/services/users/users.js
@@ -0,0 +1,56 @@
+(function() {
+  'use strict';
+
+  angular.module('api.users', [])
+  .factory('Users', function() {
+    var Users = {};
+    var userList = [
+      {
+        id: '1',
+        name: 'Joel',
+        role: 'Designer',
+        location: 'Nashville',
+        twitter: 'gijoel'
+      },
+      {
+        id: '2',
+        name: 'Jennifer',
+        role: 'Developer',
+        location: 'Chicago',
+        twitter: 'gijennifer'
+      },
+      {
+        id: '3',
+        name: 'Jill',
+        role: 'Educator',
+        location: 'Nashville',
+        twitter: 'gijill'
+      },
+      {
+        id: '4',
+        name: 'James',
+        role: 'Builder',
+        location: 'Pittsburgh',
+        twitter: 'gijames'
+      },
+      {
+        id: '5',
+        name: 'Jani',
+        role: 'Stripper',
+        location: 'Las Vegas',
+        twitter: 'gijani'
+      }
+    ];
+    Users.all = function () {
+      return userList;
+    };
+
+    Users.findById = function (id) {
+      return userList.find(function (user) {
+        return user.id === id;
+      });
+    };
+
+    return Users;
+  });
+})();
--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -1,5 +1,82 @@
 describe('Users factory', function () {
-  it('has a test spec to test 2 + 2', function () {
-    expect(2 + 2).toEqual(4);
+  var Users;
+
+  var userList = [
+    {
+      id: '1',
+      name: 'Joel',
+      role: 'Designer',
+      location: 'Nashville',
+      twitter: 'gijoel'
+    },
+    {
+      id: '2',
+      name: 'Jennifer',
+      role: 'Developer',
+      location: 'Chicago',
+      twitter: 'gijennifer'
+    },
+    {
+      id: '3',
+      name: 'Jill',
+      role: 'Educator',
+      location: 'Nashville',
+      twitter: 'gijill'
+    },
+    {
+      id: '4',
+      name: 'James',
+      role: 'Builder',
+      location: 'Pittsburgh',
+      twitter: 'gijames'
+    },
+    {
+      id: '5',
+      name: 'Jani',
+      role: 'Stripper',
+      location: 'Las Vegas',
+      twitter: 'gijani'
+    }
+  ];
+  var singleUser = {
+    id: '2',
+    name: 'Jennifer',
+    role: 'Developer',
+    location: 'Chicago',
+    twitter: 'gijennifer'
+  };
+
+  beforeEach(angular.mock.module('api.users'));
+
+  beforeEach(inject(function (_Users_) {
+    Users = _Users_;
+  }));
+
+  it('should exist', function () {
+    expect(Users).toBeDefined();
+  });
+
+  describe('.all()', function () {
+    it('should exist', function () {
+      expect(Users.all).toBeDefined();
+    });
+
+    it('should return a hard-coded list of users', function () {
+      expect(Users.all()).toEqual(userList);
+    });
+  });
+
+  describe('.findById(id)', function () {
+    it('should exist', function () {
+      expect(Users.findById).toBeDefined();
+    });
+
+    it('should return one user object if it exists', function () {
+      expect(Users.findById('2')).toEqual(singleUser);
+    });
+
+    it('should return undefined if the user cannot be found', function () {
+      expect(Users.findById('ABC')).not.toBeDefined();
+    });
   });
 });
